823fa9ed0259faffcc956999d453db494d785478
Vachek-hooves <vachek0602@gmail.com>
2024-11-19 21:32:23 +0200

    update tab bar style

--- TabNavigation/TabContainer.js
+++ TabNavigation/TabContainer.js
@@ -1,22 +1,82 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {
   TabAchievScreen,
   TabMainCollectionsScreen,
   TabUserScreen,
 } from '../screen/tab';
+
 const Tab = createBottomTabNavigator();
 
 const TabContainer = () => {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
+    <Tab.Navigator 
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: styles.tabBar,
+        tabBarShowLabel: true,
+        tabBarActiveTintColor: '#0A84FF',
+        tabBarInactiveTintColor: '#666',
+        tabBarLabelStyle: styles.tabLabel,
+        tabBarItemStyle: styles.tabBarItem,
+        tabBarIconStyle: styles.tabBarIcon,
+      }}
+    >
       <Tab.Screen
-        name="TabMainCollectionsScreen"
+        name="Collection"
         component={TabMainCollectionsScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="folder" size={46} color={color} />
+          ),
+        }}
       />
-      <Tab.Screen name="TabAchievScreen" component={TabAchievScreen} />
-      <Tab.Screen name="TabUserScreen" component={TabUserScreen} />
+      <Tab.Screen 
+        name="Progress"
+        component={TabAchievScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="trophy" size={46} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen 
+        name="Profile"
+        component={TabUserScreen}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="account" size={46} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  tabBar: {
+    backgroundColor: '#000000',
+    borderTopWidth: 0,
+    height: 100,
+    paddingBottom: 5,
+    
+  },
+  tabBarItem: {
+    paddingTop: 2,
+    height: 95,
+    // backgroundColor:'red'
+  },
+  tabLabel: {
+    fontSize: 12,
+    // marginTop: 3,
+  },
+  tabBarIcon: {
+    // backgroundColor:'red',
+    // paddingTop: 6,
+    height: 60,
+    width: 60,
+  }
+});
 
 export default TabContainer;
