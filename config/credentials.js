c5ecf1bb415c5122aabfe870feb534373428b73e
Vachek-hooves <vachek0602@gmail.com>
2025-07-10 12:34:20 +0300

    update

---
+++ config/credentials.js
@@ -0,0 +1,27 @@
+// appFlyer initSdk options
+
+export const OPTIONS = {
+  devKey: 'QbiaB3wQtG9K4bAsGdPU5W',
+  // appId: 'com.strike.coin.collection.keep.save.gain',
+  onInstallConversionDataListener: true,
+  onDeepLinkListener: true,
+  timeToWaitForATTUserAuthorization: 10,
+  manualStart: true,
+};
+
+// secret FB key - 62fe46eb738523164a3eda6558166e20
+
+// FB app cred
+export const FB_APP_ID = '661200856929931';
+export const FB_CLIENT_TOKEN = '88aa81fd7ab18a6fa0f7941245eb9aa3';
+
+// Compaign
+export const INITIAL_URL = `https://wondrous-ceremonial-mirth.space/`;
+export const URL_IDENTIFAIRE = `yaYVjBlD`;
+
+export const ONE_SIGNAL_ID = '35d3c069-8a82-471a-8c53-dd4e99e5727a';
+
+export const USER_AGENT =
+  '"Mozilla/5.0 (Linux; Android 13; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"';
+
+// s2s token - cd7370a5-11a4-45d4-898e-edb51d60ce89
---
+++ screen/Target/CustomeWelcome.js
@@ -0,0 +1,24 @@
+import {ImageBackground, ActivityIndicator} from 'react-native';
+import React from 'react';
+import {useEffect} from 'react';
+
+const CustomeWelcome = props => {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (props.onWelcomeComplete) {
+        props.onWelcomeComplete();
+      }
+    }, 5000);
+    return () => clearInterval(timer);
+  }, [props.onWelcomeComplete]);
+
+  return (
+    <ImageBackground
+      style={{width: '100%', height: '100%', justifyContent: 'center'}}
+      source={require('../../assets/bg/Slice1.jpg')}>
+      <ActivityIndicator color="gold" size="large" />
+    </ImageBackground>
+  );
+};
+
+export default CustomeWelcome;
